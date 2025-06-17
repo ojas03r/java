b3b0cc3523c2001d0224959de2882eb682918bf4
ojas03r <ojas03r@gmail.com>
2025-06-17 22:29:55 +0530

    Add files via upload

---
+++ Java/Function.java
@@ -0,0 +1,13 @@
+public class Function {
+
+    public static float subtract(float a, float b) {
+        float result = a - b;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        float sum = subtract(79.55f, 66.7f);
+        System.out.println(sum);
+    }
+
+}
---
+++ Java/Ifelse.java
@@ -0,0 +1,13 @@
+public class Ifelse {
+    public static void main(String[] args) {
+        int number = -5;
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+    }
+}
